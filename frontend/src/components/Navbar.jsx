fc94e9f876d8913862c63071f4a18d45f660e578
Chymezy <chymezy332@gmail.com>
2024-06-29 20:02:59 -0400

    seperated index.css style from Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-import logo from '../assets/logos/logo_1.jpg';
+import logo from '../assets/images/logo.svg';
+import '../assets/styles/Navbar.css'; // Ensure this is correctly imported
 
 const Navbar = () => {
   const location = useLocation();
 
   return (
-    <AppBar position="fixed" color="default" sx={{ boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }} className="navbar">
+    <AppBar position="fixed" color="default" sx={{ boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }}>
       <Toolbar>
         <img src={logo} alt="Energy Manager Logo" style={{ height: '40px', marginRight: '16px' }} />
         <Typography variant="h6" style={{ flexGrow: 1 }}>
@@ -17,7 +18,8 @@ const Navbar = () => {
           color="inherit"
           component={NavLink}
           to="/"
-          className={({ isActive }) => (isActive ? 'active-link' : '')} /* newly added or modified */
+          exact="true"
+          className={`${location.pathname === '/' ? 'active-link' : ''} navbar-button`}
         >
           Home
         </Button>
@@ -25,7 +27,7 @@ const Navbar = () => {
           color="inherit"
           component={NavLink}
           to="/features"
-          className={({ isActive }) => (isActive ? 'active-link' : '')} /* newly added or modified */
+          className={`${location.pathname === '/features' ? 'active-link' : ''} navbar-button`}
         >
           Features
         </Button>
@@ -33,7 +35,7 @@ const Navbar = () => {
           color="inherit"
           component={NavLink}
           to="/about"
-          className={({ isActive }) => (isActive ? 'active-link' : '')} /* newly added or modified */
+          className={`${location.pathname === '/about' ? 'active-link' : ''} navbar-button`}
         >
           About
         </Button>
@@ -41,7 +43,7 @@ const Navbar = () => {
           color="inherit"
           component={NavLink}
           to="/login"
-          className={({ isActive }) => (isActive ? 'active-link' : '')} /* newly added or modified */
+          className={`${location.pathname === '/login' ? 'active-link' : ''} navbar-button`}
         >
           Login
         </Button>
